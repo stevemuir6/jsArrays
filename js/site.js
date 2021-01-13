a629813d0bdf693d02c1eed58fad25a9a615474c
stevemuir6 <68389769+stevemuir6@users.noreply.github.com>
2021-01-13 18:12:44 -0500

    showing FALSE instead of TRUE

--- js/site.js
+++ js/site.js
@@ -79,15 +79,28 @@
 function FindSumA(numbers,answer){
 
         // loop through the array and add the adjacent number and check for the answer
-    let chackValue = 0;
+    let checkValue = 0;
     let found = false;
     // [10,15,3,7] start index is 0, end index -3...... or length -1
     for (let i = 0; i <= numbers.length - 2; i++) {
         // loop over the array in the after of the current value
         for(let y = (i+1);y<=numbers.length-1;y++){
-            
+            if(answer == numbers[i] + numbers[y]){
+                found = true;
+                return found;
+            }
         }
     }
+    return found;
   }
 
- 
+ function DisplaySum(){
+     let answer = document.getElementById("sumValue").value;
+     let numArray = [10,15,3.7];
+
+    //  show the array on the screen
+    document.getElementById("numList").innerHTML = numArray.toString();
+    let found = FindSumA(numArray,answer);
+    document.getElementById("results").innerHTML = found.toString();
+
+ }
