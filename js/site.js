7af976f8d92e9bfdac413f1750591a9f74be14a2
stevemuir6 <68389769+stevemuir6@users.noreply.github.com>
2021-01-13 23:30:26 -0500

    js code still returning false :(((

--- js/site.js
+++ js/site.js
@@ -1,78 +1,76 @@
-// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// //Wire up a button with a named function
-// document.getElementById("btnSubmit").addEventListener("click", doJsWork);
-
-
-// //Main driver function for app
-// function doJsWork() {
-//     numbers = generateArray();
-//     let element = document.getElementById("result");
-//     let element2 = document.getElementById("result2");
-//     let element3 = document.getElementById("result3");
-
-//     // output1.innerHTML = "";
-//     // output2.innerHTML = "";
-//     // output3.innerHTML = "";
-
-//     //step 2 add numbers to the array
-//     let size = numbers.length;
-//     size = numbers.push(11);
-//     size = numbers.push(12);
-
-//     //step 3
-//     //remove the last number from an array
-//     //numbers.pop();
-//     //remove the first number from the array
-//     //numbers.shift();
-
-//     //step 4 - Note Returns an Array
-//     //return all numbers > 5;
-//     //return numbers = 5;
-//     //return numbers less than 5;
-//     var filtered = numbers.filter(function (value, index, arr) {
-//         return value > 5;
-//     });
-
-//     //step 5 
-//     //find a value in the array
-//     var found = numbers.find(function (value, index, arr) {
-//         return value > 5;
-//     });
-
-//     //step 6
-//     //find an item using includes 
-//     var found = numbers.includes(5);
-
-//     //step 7
-//     var findNum = parseInt(document.getElementById("findNum").value);
-//     var found = numbers.includes(findNum);
-
-//     //show the results//
-//     element3.innerHTML = found;
-
-//     for (let index = 0; index < numbers.length; index++) {
-//         let num = numbers[index];
-//         element.innerHTML += `${num} `;
-//     }
-
-//     for (let index = 0; index < filtered.length; index++) {
-//         let num = filtered[index];
-//         element2.innerHTML += `${num} `;
-//     }
+//Wire up a button with a named function
+document.getElementById("btnSubmit").addEventListener("click", doJsWork);
 
 
+//Main driver function for app
+function doJsWork() {
+    numbers = generateArray();
+    let element = document.getElementById("result");
+    let element2 = document.getElementById("result2");
+    let element3 = document.getElementById("result3");
 
-// }
+    // output1.innerHTML = "";
+    // output2.innerHTML = "";
+    // output3.innerHTML = "";
 
-// function generateArray(len) {
-//     const ARRAY_LENGTH = parseInt(document.getElementById("arrayNum").value);
-//     const randomArray = [];
-//     for (let i = 0; i < ARRAY_LENGTH; i++) {
-//         randomArray.push(Math.floor(Math.random() * 100) + 1);
-//     }
-//     return randomArray;
-// }
+    //step 2 add numbers to the array
+    let size = numbers.length;
+    size = numbers.push(11);
+    size = numbers.push(12);
+
+    //step 3
+    //remove the last number from an array
+    //numbers.pop();
+    //remove the first number from the array
+    //numbers.shift();
+
+    //step 4 - Note Returns an Array
+    //return all numbers > 5;
+    //return numbers = 5;
+    //return numbers less than 5;
+    var filtered = numbers.filter(function (value, index, arr) {
+        return value > 5;
+    });
+
+    //step 5 
+    //find a value in the array
+    var found = numbers.find(function (value, index, arr) {
+        return value > 5;
+    });
+
+    //step 6
+    //find an item using includes 
+    var found = numbers.includes(5);
+
+    //step 7
+    var findNum = parseInt(document.getElementById("findNum").value);
+    var found = numbers.includes(findNum);
+
+    //show the results//
+    element3.innerHTML = found;
+
+    for (let index = 0; index < numbers.length; index++) {
+        let num = numbers[index];
+        element.innerHTML += `${num} `;
+    }
+
+    for (let index = 0; index < filtered.length; index++) {
+        let num = filtered[index];
+        element2.innerHTML += `${num} `;
+    }
+}
+
+
+function generateArray(len) {
+    const ARRAY_LENGTH = parseInt(document.getElementById("arrayNum").value);
+    const randomArray = [];
+    for (let i = 0; i < ARRAY_LENGTH; i++) {
+        randomArray.push(Math.floor(Math.random() * 100) + 1);
+    }
+    return randomArray;
+}
   
 // for example given the numbers [10, 15, 3, 7] and K of 17, return true since 10 * 7 =17.
 // Brute Force obvious way using 2 loops
@@ -100,7 +98,44 @@ function FindSumA(numbers,answer){
 
     //  show the array on the screen
     document.getElementById("numList").innerHTML = numArray.toString();
-    let found = FindSumA(numArray,answer);
+    let found = FindSumB(numArray,answer);
     document.getElementById("results").innerHTML = found.toString();
 
  }
+// ********************************
+// |\|\|\|\|\|\|\|\|\\\\\\\|\\\|||\|\ go through array in one pass    Sorted array method
+
+function FindSumB(numbers,answer){
+let found = false; 
+// sort the array
+    numbers.sort(function(a,b){
+       return a-b
+    });
+    let startIndex = 0;
+    let endIndex = numbers.length - 1;
+    do{
+        checkValue = numbers[startIndex] + numbers[endIndex];
+        if(answer == checkValue){
+            found = true;
+            break;
+        }
+        else if (answer > checkValue){
+             startIndex ++;     
+        }
+        else if (answer < checkValue){
+            endIndex --;
+        }
+
+    } while ( found == false && startIndex < endIndex );
+
+    return found;
+}
+
+
+
+
+// for(i=0;i<=Array.length; i ++){
+//   for(j=0;j<=Array.length; j ++)
+//       if (i!=j)
+//        if (array[i] + array[j]= 17)
+// }
